47414c0b058872a7462890999752b605a5e2d65f
Gangan <156800972+0xdfkoikoi@users.noreply.github.com>
2025-10-26 22:51:03 +0700

    Create SendIcon.tsx

---
+++ components/icons/SendIcon.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const SendIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" {...props}>
+    <path d="M3.105 2.289a.75.75 0 0 0-.826.95l1.414 4.949a.75.75 0 0 0 .95.545l3.879-1.102a.75.75 0 0 1 0 1.39l-3.879 1.102a.75.75 0 0 0-.95.545l-1.414 4.949a.75.75 0 0 0 .826.95 28.896 28.896 0 0 0 15.293-7.154.75.75 0 0 0 0-1.115A28.897 28.897 0 0 0 3.105 2.289Z" />
+  </svg>
+);
