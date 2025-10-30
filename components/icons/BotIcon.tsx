ec58cc125ac83c0fa98dd7c847b60114765c065a
Gangan <156800972+0xdfkoikoi@users.noreply.github.com>
2025-10-30 22:18:52 +0700

    Delete components directory

--- components/icons/BotIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const BotIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 21v-1.5M15.75 3v1.5M12 4.5v-1.5m0 18v-1.5M5.636 5.636l-1.06-1.06m16.97 0l-1.06 1.06M5.636 18.364l-1.06 1.06m16.97 0l-1.06-1.06M12 6a6 6 0 1 0 0 12 6 6 0 0 0 0-12Z" />
-  </svg>
-);
